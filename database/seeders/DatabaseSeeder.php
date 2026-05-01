b70d3165ca14b222a0b461c6fd10c1eec772a522
Win Raja Rahmatillah <win.240170226@mhs.unimal.ac.id>
2026-05-01 19:58:02 +0700

    add default user seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,23 +2,18 @@
 
 namespace Database\Seeders;
 
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     *
-     * @return void
-     */
-    public function run()
+    public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        User::create([
+            'name' => 'Mahasiswa',
+            'email' => 'win.240170226@mhs.unimal.ac.id',
+            'password' => Hash::make('password'),
+        ]);
     }
 }
